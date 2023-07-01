bffe62ddd4b11923ce9329b8d88c8207943666a1
CBeauford5 <103590886+CBeauford5@users.noreply.github.com>
2023-07-01 13:07:17 -0400

    Create signup-view.jsx

---
+++ src/components/signup-view/signup-view.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+
+export const SignupView = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      username: username,
+      password: password,
+      email: email,
+      birthDate: birthDate
+    };
+
+    fetch("https://my-movie-database-api-b1811320c6f7.herokuapp.com/users", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then((response) => {
+      if (response.ok) {
+        alert("Sign up succesfull");
+        window.location.reload();
+      } else {
+        alert("Sign up failed");
+      }
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          minLength="3"
+        />
+      </label>
+
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </label>
+
+      <label>
+        Birthday:
+        <input
+          type="date"
+          value={birthDate}
+          onChange={(e) => setBirthDate(e.target.value)}
+          required
+        />
+      </label>
+
+      <button type="submit">Submit</button>
+
+    </form>
+  );
+};
